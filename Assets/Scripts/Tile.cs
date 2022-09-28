88e7d57c5a103174e8cc9b188bbbf506e372c7e1
Steven Verborgh <steven@verborgh.be>
2022-09-28 10:54:41 +0200

    Added (un)highlight functionality to Tiles

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,17 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Renderer))]
 public class Tile : MonoBehaviour, IPointerClickHandler
 {
+
+    [SerializeField]
+    private Material _defaultMaterial;
+
+    [SerializeField]
+    private Material _highlightMaterial;
+
     private Board _board;
+    private Renderer _renderer;
 
     private void OnEnable()
-        =>  _board = FindObjectOfType<Board>();
-   
+    {
+        _board = FindObjectOfType<Board>();
+        _renderer = GetComponentInChildren<Renderer>();
+    }
+
 
     public void OnPointerClick(PointerEventData eventData)
-        =>  _board.Select(this);
+        => _board.Select(this);
 
     public void Highlight()
-        => Debug.Log($"Tile {gameObject.name} Highlighted");
+        => _renderer.material = _highlightMaterial;
+
+    internal void UnHighlight()
+        => _renderer.material = _defaultMaterial;
 }
